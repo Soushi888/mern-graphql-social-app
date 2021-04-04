c743aa02fc358179049abeeb46f29d2fa343d7b0
Soushi888 <sacha.pignot@protonmail.com>
2021-04-04 02:28:09 -0400

    ajouts variables environnement

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
+const mongoose = require("mongoose");
 
 const typeDefs = gql`
   type Query {
@@ -9,7 +11,7 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    sayHi: () => "hello world !",
+    sayHi: () => "hello world !!!!",
   },
 };
 
@@ -17,6 +19,8 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
+mongoose.connect(process.env.DATABASE_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 server.listen({ port: 3000 }).then((res) => {
   console.log(`Server running at ${res.url}`);
 });
