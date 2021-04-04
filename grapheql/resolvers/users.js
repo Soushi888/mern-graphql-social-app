fd1ef11ae7a345f3557431a3258addb61bc82177
Soushi888 <sacha.pignot@protonmail.com>
2021-04-04 05:33:35 -0400

    Login validation and error handling

--- grapheql/resolvers/users.js
+++ grapheql/resolvers/users.js
@@ -2,15 +2,54 @@ const User = require("../../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { UserInputError } = require("apollo-server");
-const { validateRegisterInput } = require("../../utils/validators");
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../utils/validators");
+
+const generateToken = (user) =>
+  jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    process.env.SECRET_KEY,
+    {
+      expiresIn: "1h",
+    }
+  );
 
 module.exports = {
   Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+
+      const user = await User.findOne({ username });
+      if (!user) {
+        errors.general = "User not founded";
+        throw new UserInputError("User not founded", { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = "Wrong credentials";
+        throw new UserInputError("Wrong credentials", { errors });
+      }
+
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
+
     async register(
       _,
       { registerInput: { username, email, password, confirmPassword } }
     ) {
-      // TODO Validate user data
       const { valid, errors } = validateRegisterInput(
         username,
         email,
@@ -41,17 +80,7 @@ module.exports = {
 
       const res = await newUser.save();
 
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username,
-        },
-        process.env.SECRET_KEY,
-        {
-          expiresIn: "1h",
-        }
-      );
+      const token = generateToken(res);
 
       return {
         ...res._doc,
--- utils/validators.js
+++ utils/validators.js
@@ -27,3 +27,15 @@ module.exports.validateRegisterInput = (
 
   return { errors, valid: Object.keys(errors).length < 1 };
 };
+
+module.exports.validateLoginInput = (username, password) => {
+  const errors = {};
+  if (username.trim() === "") {
+    errors.username = "Email must not be empty";
+  }
+  if (password.trim() === "") {
+    errors.password = "Password must not be empty";
+  }
+
+  return { errors, valid: Object.keys(errors).length < 1 };
+};
