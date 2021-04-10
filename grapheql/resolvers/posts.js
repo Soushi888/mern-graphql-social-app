21ff271572be2393544382be318a4a72221e28dc
Soushi888 <Sacha.pignot@protonmail.com>
2021-04-09 23:14:40 -0400

    add likePost() resolver

--- grapheql/resolvers/posts.js
+++ grapheql/resolvers/posts.js
@@ -1,6 +1,6 @@
 const Post = require("../../models/Post");
 const checkAuth = require("../../utils/check-auth");
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
 module.exports = {
   Query: {
@@ -24,6 +24,7 @@ module.exports = {
       }
     },
   },
+
   Mutation: {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
@@ -51,6 +52,25 @@ module.exports = {
         }
       } catch (e) {
         throw new Error(e);
+      }
+    },
+
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+      const post = await Post.findById(postId);
+
+      if (post) {
+        if (post.likes.find((l) => l.username === username)) {
+          // Post already liked
+          post.likes = post.likes.filter((l) => l.username !== username);
+        } else {
+          // Post not liked yet
+          post.likes.push({ username, createdAt: new Date().toISOString() });
+        }
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError("Post not found");
       }
     },
   },
