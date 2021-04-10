0c10055d33f49ec4344e61e37058f9c8b8d8142c
Soushi888 <Sacha.pignot@protonmail.com>
2021-04-10 00:50:32 -0400

    CSS and Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World !</h1>
-    </div>
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Router>
   );
 }
 
